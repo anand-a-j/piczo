20f2313e99ad597218bb4dda35fccaeb0e8949eb
Anand A J <anandjayakumar2001@gmail.com>
2023-09-13 14:29:07 +0530

    fix privacy policy overflow

--- lib/screens/settings_screen/widgets/privacy_policy.dart
+++ lib/screens/settings_screen/widgets/privacy_policy.dart
@@ -7,26 +7,28 @@ void privacyPolicySheet(BuildContext context) {
       backgroundColor: kBgGrey,
       context: context,
       builder: (context) {
-        return Container(
-          margin: const EdgeInsets.all(10),
-          padding: const EdgeInsets.all(10),
-          width: double.infinity,
-          child: const Column(
-            children: [
-              Text(
-                "Privacy Policy",
-                style: TextStyle(
-                    color: kWhite, fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                privacyPolicy,
-                style: TextStyle(color: kWhite),
-                textAlign: TextAlign.justify,
-              )
-            ],
+        return SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            width: double.infinity,
+            child: const Column(
+              children: [
+                Text(
+                  "Privacy Policy",
+                  style: TextStyle(
+                      color: kWhite, fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  privacyPolicy,
+                  style: TextStyle(color: kWhite),
+                  textAlign: TextAlign.justify,
+                )
+              ],
+            ),
           ),
         );
       });
